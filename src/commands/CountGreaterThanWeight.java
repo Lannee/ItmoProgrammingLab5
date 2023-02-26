d2ebc9c5a8944923ed72d10d03933abdfa219cf3
Lannee <3lirris@gmail.com>
2023-02-26 23:23:16 +0300

    count_greater_than_weight command with wierd but working realization

--- src/commands/CountGreaterThanWeight.java
+++ src/commands/CountGreaterThanWeight.java
@@ -1,6 +1,11 @@
 package main.java.src.commands;
 
 import main.java.src.Program;
+import main.java.src.logic.data.DataManager;
+import main.java.src.utils.StringConverter;
+
+import java.lang.reflect.Field;
+import java.util.concurrent.atomic.AtomicLong;
 
 public class CountGreaterThanWeight implements Command {
 
@@ -11,6 +16,26 @@ public class CountGreaterThanWeight implements Command {
         Program program = Program.getInstance();
         Command.checkArgsConformity(args, args());
 
+        DataManager<?> collection = program.collection;
+        AtomicLong counter = new AtomicLong();
+        try {
+            Field weightField = collection.getClT().getDeclaredField(args()[0]);
+            weightField.setAccessible(true);
+            Comparable givenValue = (Comparable) StringConverter.methodForType.get(weightField.getType()).apply(args[0]);
+            collection.forEach(e -> {
+                try {
+                    if(givenValue.compareTo((Comparable)weightField.get(e)) < 0) counter.getAndIncrement();
+                } catch (IllegalAccessException impossible) { }
+            });
+
+            program.out.print(counter + "\n");
+
+        } catch (NoSuchFieldException e) {
+            program.out.print("Stored type does not have " + args()[0] + " field\n");
+        } catch (NumberFormatException e) {
+            program.out.print("Incorrect given value\n");
+        }
+
     }
 
     @Override
@@ -20,6 +45,6 @@ public class CountGreaterThanWeight implements Command {
 
     @Override
     public String getDescription() {
-        return "Выводит количество элементов, значение поля " + args[0] + " которых больше заданного";
+        return "Print the number of elements whose " + args[0] + " field value is greater than the specified one";
     }
 }
